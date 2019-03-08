75e60e456e6bdf55ec353be24c399a06eeb953e3
Mux-Mastermann <43514294+Mux-Mastermann@users.noreply.github.com>
2019-03-08 21:03:55 +0100

    Some more commenting

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ document.querySelector(".img1").setAttribute("src", "images/dice"+dice1+".png");
 var dice2 = randomGen();
 document.querySelector(".img2").setAttribute("src", "images/dice"+dice2+".png");
 
+//see who is the winner
 if (dice1 > dice2) {
   var winner = "🚩 Player 1 Wins!";
 } else if (dice2 > dice1) {
@@ -20,4 +21,5 @@ if (dice1 > dice2) {
   var winner = "Draw!";
 }
 
+//print winner to heading
 document.querySelector("h1").textContent = winner;
