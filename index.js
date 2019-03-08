dbd6db1e60b8979d782fe8fab5fc69b7e80afff3
Mux-Mastermann <43514294+Mux-Mastermann@users.noreply.github.com>
2019-03-08 20:59:32 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+//random Number generator
+function randomGen() {
+  var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+  return randomNumber1;
+}
+
+//Change first Dice
+var dice1 = randomGen();
+document.querySelector(".img1").setAttribute("src", "images/dice"+dice1+".png");
+
+//Change second dice
+var dice2 = randomGen();
+document.querySelector(".img2").setAttribute("src", "images/dice"+dice2+".png");
+
+if (dice1 > dice2) {
+  var winner = "🚩 Player 1 Wins!";
+} else if (dice2 > dice1) {
+  var winner = "Player 2 Wins! 🚩";
+} else {
+  var winner = "Draw!";
+}
+
+document.querySelector("h1").textContent = winner;
